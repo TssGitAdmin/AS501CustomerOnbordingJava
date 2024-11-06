491be6214043366e9fb16eb32a2d0f558534dc09
RiddheshPingle <113330561+RiddheshPingle@users.noreply.github.com>
2024-11-06 17:15:05 +0530

    Update CustomerRelationDto.java
    Client unable to set properties

--- src/main/java/A501JavaSDKPackage/Models/A501/Request/CustomerRelationDto.java
+++ src/main/java/A501JavaSDKPackage/Models/A501/Request/CustomerRelationDto.java
@@ -3,14 +3,14 @@ package A501JavaSDKPackage.Models.A501.Request;
 import java.util.Date;
 
 public class CustomerRelationDto {
-    private String requestId;
-    private String customerSystemGeneratedId;
-    private String customerSourceSystemCode;
-    private String relatedPersonSourceSystemCode;
-    private String relationCode;
-    private String relationStartDate;
-    private String relationEndDate;
-    private Double shareHoldingPercentage;
-    private String notes;
+    public String requestId;
+    public String customerSystemGeneratedId;
+    public String customerSourceSystemCode;
+    public String relatedPersonSourceSystemCode;
+    public String relationCode;
+    public String relationStartDate;
+    public String relationEndDate;
+    public Double shareHoldingPercentage;
+    public String notes;
 }
 
